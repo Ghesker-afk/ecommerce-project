c10f0548bd43a81130230e79000df86ac5ce88f0
Ghesker-afk <spacexz12345@gmail.com>
2025-12-24 15:36:45 -0300

    feat: convert ecommerce project to TypeScript

---
+++ ecommerce-project-js/src/pages/checkout/CheckoutHeader.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router';
+import CheckoutLockIcon from '../../assets/images/icons/checkout-lock-icon.png';
+import MobileLogo from '../../assets/images/logo.png';
+import './CheckoutHeader.css';
+
+export function CheckoutHeader({ cart }) {
+  let totalQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    totalQuantity += cartItem.quantity;
+  });
+
+  return (
+    <div className="checkout-header">
+      <div className="header-content">
+        <div className="checkout-header-left-section">
+          <Link to="/">
+            <img className="logo" src="images/logo.png" />
+            <img className="mobile-logo" src={MobileLogo} />
+          </Link>
+        </div>
+
+        <div className="checkout-header-middle-section">
+          Checkout (
+          <Link className="return-to-home-link" to="/">
+            {totalQuantity}
+          </Link>
+          )
+        </div>
+
+        <div className="checkout-header-right-section">
+          <img src={CheckoutLockIcon} />
+        </div>
+      </div>
+    </div>
+  );
+}
